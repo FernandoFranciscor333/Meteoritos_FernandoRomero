71e6f9df8f904e7c0c476978c01758c130750eca
Fernando Francisco Romero <fernandofranciscor333@hotmail.com.ar>
2023-10-17 19:39:20 -0300

    Daño(7.2)

--- Meteoritos/Juego/Enemigos/EnemigoDummy.gd
+++ Meteoritos/Juego/Enemigos/EnemigoDummy.gd
@@ -1,7 +1,20 @@
 extends Node2D
 
-
+##Atributos
+var hitpoints:float=10
 
 func _on_body_entered(body: Node) -> void:
 	if body is Player:
 		body.destruir()
+	
+##Metodos custom	
+func recibir_danio(danio) -> void:
+	hitpoints -= danio
+	print(hitpoints)
+	if hitpoints <= 0.0:
+		queue_free()
+	
+
+
+	
+	
