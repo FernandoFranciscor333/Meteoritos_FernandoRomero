84caa054873dd645ab75c6733e7620760bd6efda
Fernando Francisco Romero <fernandofranciscor333@hotmail.com.ar>
2023-10-19 20:14:18 -0300

    fix

--- Meteoritos/Juego/Proyectiles/ProyectilNave.gd
+++ Meteoritos/Juego/Proyectiles/ProyectilNave.gd
@@ -21,7 +21,7 @@ func daniar(otro_cuerpo: CollisionObject2D) -> void:
 	
 
 ## Constructor
-func crear(pos:Vector2, dir:float, vel:float, danio_p:int) -> void:
+func crear(pos:Vector2, dir:float, vel:float, _danio_p:int) -> void:
 	position = pos
 	rotation = dir
 	velocidad = Vector2(vel,0).rotated(dir)
