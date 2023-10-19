9aa14a6d1c3ee2e641382c1bfab265a4bb36bffa
Fernando Francisco Romero <fernandofranciscor333@hotmail.com.ar>
2023-10-18 21:54:06 -0300

    escudo (8.1)

--- Meteoritos/Juego/Naves/Player/Player.gd
+++ Meteoritos/Juego/Naves/Player/Player.gd
@@ -22,6 +22,7 @@ onready var trail:Trail2D = $PuntoTrail/Trail2D
 onready var motor_sfx:Motor = $MotorSFX
 onready var colisionador:CollisionShape2D = $CollisionShape2D
 onready var impacto_sfx:AudioStreamPlayer = $ImpactoSFX
+onready var escudo:Escudo = $Escudo
 
 
 func _ready() -> void:
@@ -50,6 +51,11 @@ func _unhandled_input(event:InputEvent) -> void:
 	if(event.is_action_released("mover_adelante")
 		 or event.is_action_released("mover_atras")):
 				motor_sfx.sonido_off()
+	
+	# Control escudo
+	if event.is_action_pressed("escudo") and not escudo.get_esta_activado():
+		escudo.activar()
+	
 
 
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
