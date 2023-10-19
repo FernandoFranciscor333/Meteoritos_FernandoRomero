d781236416a655829055c3f590d410dc67daeafd
Fernando Francisco Romero <fernandofranciscor333@hotmail.com.ar>
2023-10-18 22:10:47 -0300

    desactivar escudo (8.2)

--- Meteoritos/Juego/Naves/Player/Escudo/Escudo.gd
+++ Meteoritos/Juego/Naves/Player/Escudo/Escudo.gd
@@ -1,11 +1,23 @@
 class_name Escudo
 extends Area2D
 
+## Variables Export
+export var energia:float = 8.0
+export var radio_desgaste:float = -1.6
+
 ## Variables
 var esta_activado:bool = false setget, get_esta_activado
 
 ## Metodos
+
+func _process(delta: float) -> void:
+	energia += radio_desgaste * delta
+	
+	if energia <= 0.0:
+		desactivar()
+
 func _ready() -> void:
+	set_process(false)
 	controlar_colisionador(true)
 	
 ## Metodos Custom
@@ -13,16 +25,25 @@ func controlar_colisionador(esta_desactivado:bool) -> void:
 	$CollisionShape2D.set_deferred("disabled", esta_desactivado)
 	
 func activar() -> void:
+	if energia <= 0.0:
+		return
+	
 	esta_activado = true
 	controlar_colisionador(false)
 	$AnimationPlayer.play("activandose")
 	
+func desactivar() -> void:
+	set_process(false)
+	esta_activado = false
+	controlar_colisionador(true)
+	$AnimationPlayer.play_backwards("activandose")
+	
 	
 ## Señales internas
-
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
-	if anim_name == "activandose":
+	if anim_name == "activandose" and esta_activado:
 		$AnimationPlayer.play("activado")
+		set_process(true)
 		
 ## Setters y Getters
 func get_esta_activado() -> bool:
