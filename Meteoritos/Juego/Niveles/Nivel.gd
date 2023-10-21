7a3efe7eab931df4b04b28a3a359981b0bf32dd4
Fernando Francisco Romero <fernandofranciscor333@hotmail.com.ar>
2023-10-21 19:38:40 -0300

    Meteoritos (1.2)

--- Meteoritos/Juego/Niveles/Nivel.gd
+++ Meteoritos/Juego/Niveles/Nivel.gd
@@ -3,9 +3,11 @@ extends Node2D
 
 ## Atributos Onready
 onready var contenedor_proyectiles:Node
+onready var contenedor_meteoritos:Node
 
 ## Atributos Export
 export var explosion:PackedScene = null
+export var meteorito:PackedScene = null
 
 ## Metodos
 func _ready() -> void:
@@ -16,11 +18,15 @@ func _ready() -> void:
 func conectar_seniales() -> void:
 	Eventos.connect("disparo",self, "_on_disparo")
 	Eventos.connect("nave_destruida", self, "_on_nave_destruida")
+	Eventos.connect("spawn_meteorito", self, "_on_spawn_meteoritos")
 	
 func crear_contenedores() -> void:
 	contenedor_proyectiles = Node.new()
 	contenedor_proyectiles.name = "ContenedorProyectiles"
 	add_child(contenedor_proyectiles)
+	contenedor_meteoritos = Node.new()
+	contenedor_meteoritos.name = "ContenedorMeteoritos"
+	add_child(contenedor_meteoritos)
 
 func _on_disparo(proyectil:Proyectil) -> void:
 	add_child(proyectil)
@@ -31,6 +37,15 @@ func _on_nave_destruida(posicion: Vector2, num_explosiones:int) -> void:
 		new_exposion.global_position = posicion
 		add_child(new_exposion)
 		yield(get_tree().create_timer(0.6), "timeout")
+
+## Conexión señales externas
+func _on_spawn_meteoritos(pos_spawn:Vector2,dir_meteorito:Vector2) -> void:
+	var new_meteorito:Meteorito = meteorito.instance()
+	new_meteorito.crear(
+		pos_spawn,
+		dir_meteorito
+	)
+	contenedor_meteoritos.add_child(new_meteorito)
 
 
 
